db6c24a99cf058daa07b6f3b2216f379b3444fe9
Pirvan Evgheni <eparvan@gmail.com>
2025-10-02 09:29:25 +0300

    Add project files.

---
+++ AppMinimMaxim/DigitAnalyzer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AppMinimMaxim
+{
+    public class DigitAnalyzer: DependencyObject
+    {
+        public static readonly DependencyProperty NumberProperty =
+            DependencyProperty.Register(
+                nameof(Number),
+                typeof(string),
+                typeof(DigitAnalyzer),
+                new PropertyMetadata(string.Empty, OnNumberChanged)
+                );
+
+        private static void OnNumberChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+           var num = (DigitAnalyzer)d;
+           var s = e.NewValue as string ?? string.Empty;
+            var digit = s.Where(char.IsDigit).Select(c=>c - '0').ToList();
+
+            if (digit.Count==0)
+            {
+                num.MaxDigit = null;
+                num.MinDigit = null;
+                return;
+            }
+            num.MaxDigit = digit.Max();
+            num.MinDigit= digit.Min();
+        }
+
+        public string Number
+        {
+            get=> (string)GetValue(NumberProperty);
+            set => SetValue(NumberProperty, value);
+        }
+
+        public static readonly DependencyProperty MaxDigitProperty =
+            DependencyProperty.Register(
+                nameof(MaxDigit),
+                typeof(int?),
+                typeof(DigitAnalyzer),
+                new PropertyMetadata(null)
+                );
+        public int? MaxDigit
+        {
+            get=> (int?)GetValue(MaxDigitProperty);
+            private set => SetValue(MaxDigitProperty, value);
+        }
+
+        public static readonly DependencyProperty MinDigitProperty =
+            DependencyProperty.Register(
+                nameof(MinDigit),
+                typeof(int?),
+                typeof(DigitAnalyzer),
+                new PropertyMetadata(null)
+                );
+        public int? MinDigit
+        {
+            get=> (int?)GetValue(MinDigitProperty);
+            set=> SetValue(MinDigitProperty, value);
+        }
+
+        public static readonly DependencyProperty ResultTextProperty =
+            DependencyProperty.Register(
+                nameof(ResultText),
+                typeof(string),
+                typeof(DigitAnalyzer),
+                new PropertyMetadata(null)
+                );
+        public string ResultText
+        {
+            get => (string)GetValue(ResultTextProperty);
+            set=> SetValue(ResultTextProperty, value);
+        }
+    }
+}
---
+++ AppMinimMaxim/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppMinimMaxim
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DigitAnalyzer Logic => (DigitAnalyzer)Resources["Logic"];
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private static readonly Regex _allowed = new(@"^[0-9+\-\s]*$");
+        private void txtBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !_allowed.IsMatch(e.Text);
+        }
+
+        private void Maxim_Click(object sender, RoutedEventArgs e)
+        {
+            if (Logic.MaxDigit is int max)
+            {
+                Logic.ResultText = $"Cifra maxima: {max}";
+            }
+            else
+            {
+                Logic.ResultText = "Nu au fost gasite cifre in text!";
+            }
+        }
+
+        private void Minim_Click(object sender, RoutedEventArgs e)
+        {
+            if (Logic.MinDigit is int min)
+            {
+                Logic.ResultText = $"Cifra minima: {min}";
+            }
+            else
+            {
+                Logic.ResultText = "Nu au fost gasite cifre in text!";
+            }
+        }
+    }
+}
